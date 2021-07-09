bd08508193a04cfcd65b239d80a12bde673dbccf
arielBWong <ariel.bingwang@outlook.com>
2021-07-09 23:30:47 +1000

    imaging pypi

--- install_deps.sh
+++ install_deps.sh
@@ -4,4 +4,4 @@
 
 conda install -y python=3.7.*
 conda install -y numpy scipy matplotlib scikit-learn
-pip3 install pydoe pygmo optproblems diversipy pymop
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pydoe pygmo optproblems diversipy pymop
