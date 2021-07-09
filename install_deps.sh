2aaf409b57b9a024820ce1574bb7a86a59c5837c
Bing Wang <ariel.bingwang@outlook.com>
2021-07-09 20:56:33 +1000

    Initial commit
    Signed-off-by: Bing Wang <ariel.bingwang@outlook.com>

---
+++ install_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Run this program to install all required dependencies in conda environment
+
+conda install -y python=3.7.*
+conda install -y numpy scipy matplotlib scikit-learn
+pip3 install -y pydoe pygmo optproblems diversipy
