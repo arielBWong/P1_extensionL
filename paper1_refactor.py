b3520529aa9bc5487c4b09581ccf98a7e6a07163
arielBWong <ariel.bingwang@outlook.com>
2021-07-11 21:35:29 +1000

    fix os.path.exists bug

--- paper1_refactor.py
+++ paper1_refactor.py
@@ -963,7 +963,7 @@ def paper1_mainscript(seed_index, target_problem, method_selection, search_ideal
     tmp_path = os.path.join(tmp_path, 'results_OBJ%d' % target_problem.n_obj)
     tmp_folder = os.path.join(tmp_path, '%s_%s_%d'%(target_problem.name(), method_selection,int(search_ideal)))
     tmp_savename = os.path.join(tmp_folder, 'trainy_seed_%d.csv' % seed_index)
-    if os.path.exist(tmp_savename):
+    if os.path.exists(tmp_savename):
         print(tmp_savename + 'exists and pass run')
         return
 
